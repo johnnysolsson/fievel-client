1d2559a8f582c1d6af3a80dccde82a81995c34f6
Johnny <johnnysolsson@gmail.com>
2025-11-27 15:27:03 +0100

    Updated deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/', // important for correct asset paths at domain root
   build: {
+    outDir: 'dist',    // default for Vite, but explicit for clarity
+    assetsDir: 'assets', // optional, keeps assets organized
     sourcemap: true
   }
 });
